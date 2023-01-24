95b0d05311f64e5744d3cd12fde56645af63b0fe
Javier Salas <nestorsalas.p@gmail.com>
2023-01-23 19:18:30 -0500

    Se agregan filtros y funcionalidades al Todo APP

--- src/componentes/TodoInput/TodoInput.jsx
+++ src/componentes/TodoInput/TodoInput.jsx
@@ -1,4 +1,15 @@
-const TodoInput = () => {
+import { useState } from "react"
+
+const TodoInput = ({ addTodo }) => {
+    const [title, setTitle] = useState('')
+
+    const handleTodo = (e) => {
+        if (e.key.toLowerCase() === 'enter') {
+            addTodo(title)
+            setTitle('')
+        }
+    }
+
     return (
         <div className="mt-6 relative">
             <div className="absolute inset-y-0 left-2 flex items-center pointer-events-none"  >
@@ -7,8 +18,10 @@ const TodoInput = () => {
             <input 
                 type="text" 
                 className="focus:shadow-lg font-inter focus:shadow-text_secundary pl-12 w-full py-4 bg-gray-700 rounded-xl outline-none transition-all duration-300 easy-in-out"
-                placeholder="Cuál es el siguiente...">    
-            </input>
+                placeholder="Cuál es el siguiente..."
+                value={title}
+                onChange={e => setTitle(e.target.value)} 
+                onKeyDown={e => handleTodo(e)} />
         </div>
     )
 }
--- src/componentes/TodoList/TodoList.jsx
+++ src/componentes/TodoList/TodoList.jsx
@@ -1,10 +1,27 @@
 import { TodoFilters } from "../TodoFilters";
+import { Todo } from "../Todo/Todo"
 
-const TodoList = ({ children }) => {
+const TodoList = ({ todos, handleSetComplete, handleDelete, activeFilter, showActiveTodos, showAllTodos, showCompleted, handleClearComplete }) => {
     return (
         <div className="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2-xl">
-            {children}
-            <TodoFilters/>
+            {todos.map(todo => {
+                return (
+                    <Todo 
+                        key={todo.id} 
+                        todo={todo} 
+                        handleSetComplete={handleSetComplete}
+                        handleDelete={handleDelete}
+                    />
+                )
+            })}
+            <TodoFilters 
+            activeFilter={activeFilter}
+            total={todos.length}
+            showActiveTodos={showActiveTodos}
+            showAllTodos={showAllTodos}
+            showCompleted={showCompleted}
+            handleClearComplete={handleClearComplete}
+            />
         </div>
     )
 
